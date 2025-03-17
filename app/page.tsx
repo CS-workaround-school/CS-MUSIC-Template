94a45473c9c991ef5fb757f90afaaf6e1dbb831b
stevenxngo <83997026+stevenxngo@users.noreply.github.com>
2025-03-17 11:21:42 -0700

    Dynamic font and margin size

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,17 @@ export default function Home() {
     link.download = "album_cover.png";
     link.click();
   };
+
+  let fontSize = 5;
+  let margin = 3;
+  if (inputText.length > 15) {
+    fontSize = 3;
+    margin = 1;
+  } else if (inputText.length > 10) {
+    fontSize = 4;
+    margin = 2;
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="font-swiss911 mb-4 text-3xl">I AM MUSIC GENERATOR</h1>
@@ -24,8 +35,10 @@ export default function Home() {
         className="relative inline-flex h-[400px] w-[400px] items-center justify-center border-2 bg-white shadow-lg"
       >
         {inputText.split("").map((char, index) => (
-          <div key={index} className="relative mr-3">
-            <span className="font-optispire absolute top-[48%] left-1/2 origin-center -translate-x-1/2 -translate-y-1/2 scale-x-[2.4] scale-y-[2.2] text-5xl leading-1 font-light">
+          <div key={index} className={`mr-${margin} relative`}>
+            <span
+              className={`text-${fontSize}xl font-optispire absolute top-[48%] left-1/2 origin-center -translate-x-1/2 -translate-y-1/2 scale-x-[2.4] scale-y-[2.2] leading-1 font-light`}
+            >
               {char === " " ? (
                 <span className="whitespace-pre"> </span>
               ) : (
@@ -33,7 +46,9 @@ export default function Home() {
               )}
             </span>
             <div className="relative inline-flex origin-center translate-x-0.5 scale-x-[3] items-center justify-center">
-              <span className="font-swiss911 relative origin-center scale-x-[0.45] scale-y-[0.9] text-5xl font-bold">
+              <span
+                className={`text-${fontSize}xl font-swiss911 relative origin-center scale-x-[0.45] scale-y-[0.9] font-bold`}
+              >
                 {char === " " ? (
                   <span className="whitespace-pre"> </span>
                 ) : (
@@ -55,7 +70,7 @@ export default function Home() {
       />
 
       <button
-        className="mt-4 rounded-sm bg-black px-4 py-2 text-white"
+        className="font-swiss911 mt-4 cursor-pointer rounded-sm bg-black px-4 py-2 text-white hover:bg-gray-800 focus:ring-2 focus:ring-gray-400 focus:outline-none"
         onClick={downloadImage}
       >
         SAVE
